9a8baf4786e046340432a50df365cb2cda38bb4b
Shazia <n_kirmani@yahoo.com>
2019-10-16 12:10:59 +0100

    Learning components

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+class Header extends React.Component {
+    render(){
+        return(
+            <h1>My ToDo List</h1>
+        );
+    }
+}
+
+export default Header;
---
+++ src/components/Intro.js
@@ -0,0 +1,10 @@
+import React from 'react';
+class Intro extends React.Component {
+    render(){
+        return(
+            <p>Welcome to my new app</p>
+        );
+    }
+}
+
+export default Intro;
