9eb305ce9ed02facf061ee79ef248c67a5ccd691
Shazia <n_kirmani@yahoo.com>
2019-10-26 11:11:29 +0100

    no error now but list no lay out

--- src/App.js
+++ src/App.js
@@ -3,17 +3,15 @@ import './App.css';
 import Header from './components/Header';
 import Intro from './components/Intro';
 import Task from './components/Task';
-import Media from './components/Media';
+import Media from './components/Task';
 
 
 function App() {
   return (
  <div className="container">
  <Header />
+ <Media imageAdded="scenic view " />
  <Intro introDescription ="remaining task"/>
- <Media image ="https://assets.simpleviewinc.com/simpleview/image/upload/c_fill,h_440,q_50,w_1000/v1/clients/anchorage/highway-deyoung-state_edited_a6dbb76b-6b90-43ca-add5-6464ba6e9536.jpg"
-     alt="scenic view " />
-
  <Task taskDescription="Iman's PTA @ 6pm"/>
  <Task taskDescription="make orthodontist appointment for Arryan"/>
  <Task taskDescription="book theory test for Areesh"/>
