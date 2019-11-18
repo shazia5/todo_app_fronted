02c5c6760f5d584e97d114821324528335f3a184
Shazia <n_kirmani@yahoo.com>
2019-11-18 11:52:12 +0000

    img

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,7 +10,7 @@ class Footer extends React.Component {
         src="https://assets.simpleviewinc.com/simpleview/image/upload/c_fill,h_240,q_50,w_1050/v1/clients/anchorage/highway-deyoung-state_edited_a6dbb76b-6b90-43ca-add5-6464ba6e9536.jpg"
         alt="scenic view " />
 
-              </footer>
+        </footer>
                    
         )
     }
